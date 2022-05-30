bb7106fc9c7a30061d7fd7b5970427ae62aa1d84
blldyyd <88305201+blldyyd@users.noreply.github.com>
2022-05-30 09:31:10 +0800

    Update reuse.py

--- reuse/reuse.py
+++ reuse/reuse.py
@@ -1,21 +1,26 @@
+
 from pandas import DataFrame
 
 
 def read_csv(address: str)->list[DataFrame]:
     from os import walk
     from pandas import read_csv
+
     for i, o, p in walk(address):  # 获取csv文件
         csv_names = [x for x in p if x[-4:] == '.csv']
-        data = [read_csv(i + x) for x in csv_names]
-
-        return data
+        listdata = [read_csv(i + x) for x in csv_names]
+        listdata = clean_data(listdata)
+        return listdata
 
 def read_xlsx(address: str)->list[DataFrame]:
     from os import walk
     from pandas import read_excel
     for i, o, p in walk(address):  # 获取csv文件
         csv_names = [x for x in p if x[-4:] == '.xls' or x[-5:] == '.xlsx']
-        data = [read_excel(i + x) for x in csv_names]
-        
-        return data
+        listdata = [read_excel(i + x) for x in csv_names]
+        listdata=clean_data(listdata)
+        return listdata
 
+def clean_data(listdata:DataFrame)->list[DataFrame]:
+    listdata=[i.dropna() for i in listdata]
+    return listdata
